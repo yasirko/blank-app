0ce1b4efb8e8b95c547fa66165561477e798be64
yasirko <41874535+yasirko@users.noreply.github.com>
2024-11-09 11:24:31 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -239,7 +239,9 @@ def check_tickers(tickers, percentage):
             
             # Check if current price is between lowest price and lowest price * multiplier
             if lowest_price <= current_price <= lowest_price * multiplier:
-                results[ticker] = {
+                # Remove '.SR' if the ticker is a number
+                display_ticker = ticker.replace('.SR', '') if ticker.replace('.SR', '').isdigit() else ticker
+                results[display_ticker] = {
                     'الاسم': ticker_to_arabic_name.get(ticker, 'غير معروف'),
                     'السعر الحالي': round(current_price, 2),
                     'القاع السنوي': round(lowest_price, 2)
